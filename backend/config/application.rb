6b811e778a4e8a2e7180469a10ed43a94d1131b1
katsuomi <mugichak1227@gmail.com>
2019-12-21 17:42:22 +0900

    3002番ポートを許可

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -26,7 +26,7 @@ module RailsCookie
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "http://localhost:3001"
+        origins "http://localhost:3002"
         resource "*",
           headers: :any,
           methods: [:get, :post, :options, :head],
